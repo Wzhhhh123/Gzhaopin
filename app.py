73f986e1dc83186cae134061dae272d7874af614
Wzhhhh123 <59770581+Wzhhhh123@users.noreply.github.com>
2025-06-30 01:02:42 +0800

    a

--- app.py
+++ app.py
@@ -186,5 +186,5 @@ def testimonials():
 def page_not_found(e):
     return render_template('404.html'), 404
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=5222)
